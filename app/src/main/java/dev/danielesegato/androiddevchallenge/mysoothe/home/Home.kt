806a81396595b65a051bf54a8a7d8169d58db198
Daniele Segato <daniele.segato@neosperience.com>
2021-03-22 19:01:57 +0100

    Add search field in home

--- app/src/main/java/dev/danielesegato/androiddevchallenge/mysoothe/home/Home.kt
+++ app/src/main/java/dev/danielesegato/androiddevchallenge/mysoothe/home/Home.kt
@@ -15,9 +15,95 @@
  */
 package dev.danielesegato.androiddevchallenge.mysoothe.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.danielesegato.androiddevchallenge.mysoothe.R
+import dev.danielesegato.androiddevchallenge.mysoothe.ui.theme.MyTheme
+
+val horizontalPadding = 16.dp
 
 @Composable
 fun Home() {
-    
+    Column {
+        Search(
+            Modifier.padding(top = 56.dp)
+        )
+    }
+}
+
+@Composable
+fun Search(modifier: Modifier = Modifier) {
+    var searchTerm by remember { mutableStateOf(TextFieldValue()) }
+
+    TextField(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(56.dp)
+            .padding(horizontal = horizontalPadding),
+        value = searchTerm,
+        onValueChange = { searchTerm = it },
+        textStyle = MaterialTheme.typography.body1,
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = MaterialTheme.colors.surface,
+            textColor = MaterialTheme.colors.onSurface,
+        ),
+        leadingIcon = {
+            Image(
+                modifier = Modifier.size(18.dp),
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_baseline_search_24),
+                colorFilter = ColorFilter.tint(MaterialTheme.colors.onSurface),
+                contentDescription = stringResource(R.string.home_field_search_leadingicon_desc),
+            )
+        },
+        placeholder = {
+            // TODO how do I center vertically the text?
+            Text(
+                text = stringResource(R.string.home_field_search_hint),
+                style = MaterialTheme.typography.body1,
+                color = MaterialTheme.colors.onSurface,
+            )
+        },
+    )
+}
+
+@Preview
+@Composable
+fun PreviewHomeLight() {
+    MyTheme(darkTheme = false) {
+        Surface(color = MaterialTheme.colors.background) {
+            Home()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewHomeDark() {
+    MyTheme(darkTheme = true) {
+        Surface(color = MaterialTheme.colors.background) {
+            Home()
+        }
+    }
 }
